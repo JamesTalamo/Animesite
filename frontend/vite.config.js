26951289454c943432594209ce042eb58da66826
James Talamo <jamestalamo4@gmail.com>
2024-11-12 22:53:51 +0800

    update 1.0.9

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,12 @@ import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
 
 dotenv.config()
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Use Render's assigned port or default to 3000
+    host: '0.0.0.0', // Make sure the server is accessible on all network interfaces
+  },
 })
