826971563be8fa96626648fdb97114edb2198b20
James Talamo <jamestalamo4@gmail.com>
2024-11-15 23:47:30 +0800

    1.3.2

--- frontend/src/components/AnimeBoxContainer.jsx
+++ frontend/src/components/AnimeBoxContainer.jsx
@@ -8,7 +8,7 @@ import { Carousel } from "antd";
 let AnimeBoxContainer = ({ anime, animeHead }) => {
 
     return (
-        <Box w={{ lg: 'container.xl', md: 'container.md', sm: 'container.sm' }} bg='gray.600' borderRadius='lg'>
+        <Box w={{ lg: 'container.xl', md: 'container.md', sm: 'container.sm' }} pb='15px' bg='gray.800' borderRadius='lg' border='1px rgba(255, 255, 255, 0.16) solid'>
             <Heading
                 align='start'
                 color='white'
