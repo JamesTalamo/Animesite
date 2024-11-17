52f37dc4060394f431395dcd92244fd9f21be028
James Talamo <jamestalamo4@gmail.com>
2024-11-17 20:02:57 +0800

    update 1.6.0

--- frontend/src/components/GenrePage/RightSideCompGenre.jsx
+++ frontend/src/components/GenrePage/RightSideCompGenre.jsx
@@ -5,7 +5,7 @@ import RightSideCradsGenre from '../GenrePage/RightSideCardsGenre'
 let RightSideCompGenre = ({ anime }) => {
     return (
         <Box  border={{ lg: '1px rgba(255, 255, 255, 0.16) solid', base: 'none' }} borderRadius='xl' >
-            <Text color='white' fontWeight='bold' fontSize='xl' p='25px'><span style={{ color: '#E53E3E' }}>T</span>op Airing Animes</Text>
+            <Text color='white' fontWeight='bold' fontSize='xl' p='25px' align='start'><span style={{ color: '#E53E3E' }}>T</span>op Airing Animes</Text>
 
             <VStack spacing='1'>
                 {anime.map((anime) => (
