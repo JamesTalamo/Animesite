8f4563788220a236fa56ecde3897bcb6017f7993
James Talamo <jamestalamo4@gmail.com>
2024-11-20 19:32:08 +0800

    update 1.7.3

--- frontend/src/components/WatchPage/ServerContainer.jsx
+++ frontend/src/components/WatchPage/ServerContainer.jsx
@@ -23,15 +23,14 @@ let ServerContainer = ({ changeServer }) => {
             p='25px'
             flexDirection={{ lg: 'row', base: 'column' }}
         >
-            <Box w={{ lg: '20%', base: '40%' }} h='100%' bg='gray.800' display='flex' alignItems='center' justifyContent='center' >
+            <Box w={{ lg: '20%', base: '90%' }} h='100%' bg='gray.800' display='flex' alignItems='center' justifyContent='center' >
                 <Text color='white' fontWeight='bold'>If current server doesn't work please try other servers.</Text>
             </Box>
 
             <Box w={{ lg: '79%', base: '100%' }} h='100%' bg='gray.800' display='flex' alignItems='start' justifyContent='space-around' flexDirection='column' p='10px' borderRadius='lg'>
 
                 <Box w='100%' h='50%' display='flex' alignItems='center' justifyContent='start'>
-                    <MdOutlineSubtitles />
-                    <Text pl='25px' color='white' fontWeight='bold'>
+                    <Text  color='white' fontWeight='bold'>
                         Sub
                     </Text>
                     <Box w='100%' h='100%' ml='25px' display='flex' alignItems='center' justifyContent='start' gap='2px'>
@@ -52,9 +51,8 @@ let ServerContainer = ({ changeServer }) => {
                 </Box>
 
                 <Box w='100%' h='50%' display='flex' alignItems='center' justifyContent='start'>
-                    <FaMicrophoneLines />
-                    <Text pl='22px' color='white' fontWeight='bold'>
-                        Dub
+                    <Text  color='white' fontWeight='bold'>
+                        Sub
                     </Text>
                     <Box w='100%' h='100%' ml='25px' display='flex' alignItems='center' justifyContent='start' gap='2px'>
 
